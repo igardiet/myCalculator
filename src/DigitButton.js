2d776b4daf6b0a00c4e05c315ff8d7f0048e4d87
Iñaki García <igardiet@gmail.com>
2023-03-14 00:18:34 +0100

    file creation and logic update

---
+++ src/DigitButton.js
@@ -0,0 +1,9 @@
+import { ACTIONS } from "./App";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button onClick={() => dispatch({ type: ACTIONS.ADD, payload: { digit } })}>
+      {digit}
+    </button>
+  );
+}
